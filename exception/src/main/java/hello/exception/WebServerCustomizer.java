c9f10d5d1c6856f20c49648dd3be99fc7435fcf4
hskhsmm <hskhsmm@naver.com>
2025-05-04 16:58:27 +0900

    feat: 서블릿 예외 처리 - 오류 페이지 작동 로그

--- exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -15,6 +15,7 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
 
+        //경로 다시 요청해서 필터 서블릿 인터셉터 해서 컨트롤러까지 가는 것
         ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
 
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
